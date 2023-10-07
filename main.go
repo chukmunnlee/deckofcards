9b70e22b468f5cb92e609905086afe72e59f6028
chukmunnlee <chukmunnlee@gmail.com>
2023-10-07 12:14:36 +0800

    Documented: GET /api/deck/:deck_id as NOT IDEMPOTENT

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 	registerGET(fmt.Sprintf("/api/deck/:%s", PARAM_DECK_ID),
 		mkApiDeckDraw(cardDecks, storage), r)
 
-	// TODO: GET /api/deck/:deck_id/contents
+	// GET /api/deck/:deck_id/contents
 	registerGET(fmt.Sprintf("/api/deck/:%s/contents", PARAM_DECK_ID),
 		mkApiDeckGetContents(cardDecks, storage), r)
 
@@ -74,11 +74,11 @@ func main() {
 	registerGET(fmt.Sprintf("/api/deck/:%s/piles", PARAM_DECK_ID),
 		mkApiPilesGet(cardDecks, storage), r)
 
-	// GET /api/deck/:deck_id/piles
+	// POST /api/deck/:deck_id/piles
 	registerPOST(fmt.Sprintf("/api/deck/:%s/piles", PARAM_DECK_ID),
 		mkApiPilesPost(cardDecks, storage), r)
 
-	// TODO: GET /api/deck/:deck_id/pile/:pile_name/contents
+	// GET /api/deck/:deck_id/pile/:pile_name/contents
 	registerGET(fmt.Sprintf("/api/deck/:%s/pile/:%s/contents", PARAM_DECK_ID, PARAM_PILE_NAME),
 		mkApiDeckGetContents(cardDecks, storage), r)
 
